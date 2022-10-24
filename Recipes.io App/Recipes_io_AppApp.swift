2e00586384d548b308e071925d086a4fbd22bc7d
Krystal Zhang <72481348+KrystalZhang612@users.noreply.github.com>
2022-10-23 22:31:36 -0700

    Initial Commit

---
+++ Recipes.io App/Recipes_io_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipes_io_AppApp.swift
+//  Recipes.io App
+//
+//  Created by erika on 10/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Recipes_io_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
