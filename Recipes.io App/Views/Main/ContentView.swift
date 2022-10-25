f255cfcf11ce0c0f285c677b619b0ed98d398fb8
krystalzhang612 <krystalzhang612@gmail.com>
2022-10-25 04:54:54 -0700

    COMMITED

--- Recipes.io App/Views/Main/ContentView.swift
+++ Recipes.io App/Views/Main/ContentView.swift
@@ -2,25 +2,21 @@
 //  ContentView.swift
 //  Recipes.io App
 //
-//  Created by erika on 10/23/22.
+//  Created by Krystal Zhang on 10/23/22.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        TabBar()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(RecipesViewModel())
     }
 }
+
